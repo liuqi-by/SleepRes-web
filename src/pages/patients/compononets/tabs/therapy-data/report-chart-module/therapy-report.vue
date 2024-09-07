3f87a74c7f8678edf364ba68f3a0fd14918c27ba
liuqi <851420437@qq.com>
2024-09-07 16:17:02 +0800

    ✨ feat: 对接静态信息字段

--- src/pages/patients/compononets/tabs/therapy-data/report-chart-module/therapy-report.vue
+++ src/pages/patients/compononets/tabs/therapy-data/report-chart-module/therapy-report.vue
@@ -13,27 +13,27 @@
                 <div class="chart-info">
                     <div class="chart-info-item">
                         <div class="chart-info-label">Usage days</div>
-                        <div class="chart-info-value">27/28(96%)</div>
+                        <div class="chart-info-value">{{ deviceReport?.use_days }}</div>
                     </div>
                     <div class="chart-info-item color-[#00CE79]">
                         <div class="chart-info-label">>= 4 hour days</div>
-                        <div class="chart-info-value">27(96%)</div>
+                        <div class="chart-info-value">{{ deviceReport?.usage_days_use }}</div>
                     </div>
                     <div class="chart-info-item color-[#FF9795]">
                         <div class="chart-info-label">{{ '< 4 hour days' }}</div>
-                        <div class="chart-info-value">27(96%)</div>
+                        <div class="chart-info-value">{{ deviceReport?.usage_days_use_not }}</div>
                     </div>
                     <div class="chart-info-item">
                         <div class="chart-info-label">Days not used</div>
-                        <div class="chart-info-value">27(96%)</div>
+                        <div class="chart-info-value">{{ deviceReport?.not_use_days }}</div>
                     </div>
-                    <div class="chart-info-item">
+                    <!-- <div class="chart-info-item">
                         <div class="chart-info-label">Days no data</div>
                         <div class="chart-info-value">27(96%)</div>
-                    </div>
+                    </div> -->
                     <div class="chart-info-item">
                         <div class="chart-info-label">Used/day(avg)</div>
-                        <div class="chart-info-value">9.3hrs</div>
+                        <div class="chart-info-value">{{ deviceReport?.usetime_avg_use }}</div>
                     </div>
                 </div>
             </div>
@@ -53,16 +53,16 @@
                         <div class="chart-info-value">24.0 L/min</div>
                     </div>
                     <div class="chart-info-item">
-                        <div class="chart-info-label">Maximum(avg)</div>
-                        <div class="chart-info-value">23.8</div>
+                        <div class="chart-info-label">Max Leak</div>
+                        <div class="chart-info-value">{{ deviceReport?.leak_max }}</div>
                     </div>
-                    <div class="chart-info-item color-[#f4c430]">
+                    <!-- <div class="chart-info-item color-[#f4c430]">
                         <div class="chart-info-label">95th %(avg)</div>
                         <div class="chart-info-value">27(96%)</div>
-                    </div>
+                    </div> -->
                     <div class="chart-info-item">
-                        <div class="chart-info-label">Median(avg)</div>
-                        <div class="chart-info-value">20.1</div>
+                        <div class="chart-info-label">Avg Leak</div>
+                        <div class="chart-info-value">{{ deviceReport?.leak_avg }}</div>
                     </div>
                 </div>
             </div>
@@ -81,15 +81,15 @@
                 <div class="chart-info">
                     <div class="chart-info-item">
                         <div class="chart-info-label">AHI</div>
-                        <div class="chart-info-value">24.0 L/min</div>
+                        <div class="chart-info-value">{{ deviceReport?.ahi }}</div>
                     </div>
                     <div class="chart-info-item color-[#b89dc4]">
                         <div class="chart-info-label">HI</div>
-                        <div class="chart-info-value">23.8</div>
+                        <div class="chart-info-value">{{ deviceReport?.hi }}</div>
                     </div>
                     <div class="chart-info-item color-[#8e62a1]">
                         <div class="chart-info-label">AI</div>
-                        <div class="chart-info-value">27(96%)</div>
+                        <div class="chart-info-value">{{ deviceReport?.ai }}</div>
                     </div>
                 </div>
             </div>
@@ -101,10 +101,11 @@
     import UsageChart from '../charts/usage-chart.vue';
     import leakChart from '../charts/leak-chart.vue';
     import AhiChart from '../charts/ahi-chart.vue';
-    import type { BarChartRes } from '~/api/report/types';
+    import type { BarChartRes, DeviceReportRes } from '~/api/report/types';
 
     defineProps<{
         barChartData: BarChartRes;
+        deviceReport: DeviceReportRes | undefined;
     }>();
 </script>
 
